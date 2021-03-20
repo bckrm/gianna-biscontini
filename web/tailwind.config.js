34fa351b58ccfdf4a7660c19a4a8b745b60db575
asmith02 <asa.smith@verizonmedia.com>
2021-03-19 22:38:06 -0400

    Add typescale to tw config

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -2,6 +2,21 @@ module.exports = {
     purge: ['./src/**/*.js', './src/**/*.jsx'],
     darkMode: false, // or 'media' or 'class'
     theme: {
+        fontSize: {
+            h1: '5.61rem',
+            h2: '4.209rem',
+            h3: '3.157rem',
+            h4: '2.369rem',
+            h5: '1.777rem',
+            h6: '1.333rem',
+            body: '1rem',
+            caption: '.75rem',
+            attribution: '.563rem',
+        },
+        container: (theme) => ({
+            center: true,
+            padding: theme('spacing.5'),
+        }),
         extend: {},
     },
     variants: {
