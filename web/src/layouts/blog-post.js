7e698524920aab54e261b3a25f780521e0ba512d
asasmith <smith.asa.la@gmail.com>
2021-03-27 18:45:28 -0400

    Add blog post template

---
+++ web/src/layouts/blog-post.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { graphql } from 'gatsby';
+import Layout from './layout';
+import BlogPost from '../components/blog/blogPost';
+
+export const query = graphql`
+    query BlogPostTemplateQuery($id: String!) {
+        post: sanityPost(id: { eq: $id }) {
+            id
+            publishedAt
+            mainImage {
+                asset {
+                    fluid(maxWidth: 500) {
+                        ...GatsbySanityImageFluid
+                    }
+                }
+            }
+            title
+            slug {
+                current
+            }
+            _rawExcerpt(resolveReferences: { maxDepth: 5 })
+            _rawBody(resolveReferences: { maxDepth: 5 })
+        }
+    }
+`;
+
+// {post && (
+//     <SEO
+//         title={post.title || 'Untitled'}
+//         description={toPlainText(post._rawExcerpt)}
+//         image={post.mainImage}
+//     />
+// )}
+
+export default function BlogPostLayout({ data }) {
+    const { post } = data;
+    return (
+        <Layout logoColor="var(--brand-1)">
+            <BlogPost data={post} />
+        </Layout>
+    );
+}
+
+BlogPostLayout.propTypes = {
+    data: PropTypes.object.isRequired,
+};
