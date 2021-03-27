c1ab3530bc021dad3547da6b194ecd93697e004b
asasmith <smith.asa.la@gmail.com>
2021-03-27 18:32:51 -0400

    Add writing page and blog post schemas to sanity

---
+++ studio/schemas/documents/writingPage.js
@@ -0,0 +1,18 @@
+export default {
+    name: 'writingPage',
+    title: 'writing Page',
+    type: 'document',
+    __experimental_actions: [
+        'create',
+        'update',
+        // 'delete',
+        'publish',
+    ],
+    fields: [
+        {
+            name: 'pageTitle',
+            title: 'Page Title',
+            type: 'string',
+        },
+    ],
+};
