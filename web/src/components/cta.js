532ab616b01393e7f88e9bc4357de3c8c8911bc1
asasmith <smith.asa.la@gmail.com>
2021-03-28 13:43:40 -0400

    Adjust cta layout for small screens

--- web/src/components/cta.js
+++ web/src/components/cta.js
@@ -10,9 +10,9 @@ export default function CTA({ data, color }) {
 
     const headingColor = color ? `text-${color}` : 'text-brand-1';
     return (
-        <section className="container mb-32 text-center">
+        <section className="container mb-12 lg:mb-32 text-center">
             <h2 className={`text-h3 ${headingColor} mb-8`}>{heading}</h2>
-            <p className="text-xl max-w-[40%] mb-8 mx-auto">{body}</p>
+            <p className="text-xl lg:max-w-[40%] mb-8 mx-auto">{body}</p>
             <InternalLink data={link} />
         </section>
     );
