7be6ddc9a3730b129684e6adf8578ca229c786a9
asasmith <smith.asa.la@gmail.com>
2021-03-28 15:34:31 -0400

    Remove styled-components dependency, show mobile nav at lg breakpoint

--- web/src/components/nav/mainNav.js
+++ web/src/components/nav/mainNav.js
@@ -4,7 +4,7 @@ import { Link } from 'gatsby';
 
 export default function MainNav({ navItems }) {
     return (
-        <ul className="hidden md:flex flex-row items-center font-bold leading-5 justify-between self-center uppercase">
+        <ul className="hidden lg:flex flex-row items-center font-bold leading-5 justify-between self-center uppercase">
             {navItems.map((item) => {
                 return (
                     <li className="ml-8 text-xl" key={item.link}>
--- web/src/components/nav/mobileNav.js
+++ web/src/components/nav/mobileNav.js
@@ -1,40 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
-import tw from 'twin.macro';
 import { Link } from 'gatsby';
-
-const NavButton = styled.button`
-    ${tw`absolute flex flex-col justify-between md:hidden h-8 w-10 right-0 top-1/2 transform -translate-y-1/2 -translate-x-4 z-10`}
-`;
-
-const StyledSpan = styled.span`
-    ${tw`h-1 rounded-full transform-gpu transition transition-all duration-300 w-full origin-right`}
-    background-color: ${({ isMobileNavOpen }) =>
-        isMobileNavOpen ? 'black' : 'black'};
-    &:nth-of-type(1) {
-        transform: ${({ isMobileNavOpen }) =>
-            isMobileNavOpen ? `rotate(-45deg)` : `rotate(0)`};
-    }
-    :nth-of-type(2) {
-        opacity: ${({ isMobileNavOpen }) => (isMobileNavOpen ? `0` : `1`)};
-        width: ${({ isMobileNavOpen }) => (isMobileNavOpen ? `0` : `100%`)};
-    }
-    :nth-of-type(3) {
-        transform: ${({ isMobileNavOpen }) =>
-            isMobileNavOpen ? `rotate(45deg)` : `rotate(0)`};
-    }
-`;
-
-const UL = styled.ul`
-    ${tw`absolute right-0 md:hidden min-h-screen w-max flex flex-col items-end transform-gpu transition pr-4 pt-32 top-0 text-right w-1/2`}
-    transform: ${({ isMobileNavOpen }) =>
-        isMobileNavOpen ? `translateX(0)` : `translateX(100%)`}
-`;
-
-const LI = styled.li`
-    ${tw`capitalize font-bold ml-8 mb-10 transition-colors duration-700 ease-in-out relative text-xl`}
-`;
 
 export default function MobileNav({
     handleNavToggle,
@@ -42,31 +8,53 @@ export default function MobileNav({
     isScrolled,
     navItems,
 }) {
+    const navActiveStyles = isMobileNavOpen
+        ? 'translate-x-0'
+        : 'translate-x-full';
+
+    const firstSpanStyles = isMobileNavOpen ? '-rotate-45' : 'rotate-0';
+    const secondSpanStyles = isMobileNavOpen
+        ? 'opacity-0 w-0'
+        : 'opacity-100 w-full';
+    const thirdSpanStyles = isMobileNavOpen ? 'rotate-45' : 'rotate-0';
+
     return (
         <>
-            <NavButton type="button" onClick={handleNavToggle}>
-                <StyledSpan
+            <button
+                className="absolute flex flex-col justify-between lg:hidden h-8 w-10 right-0 top-1/2 transform -translate-y-1/2 -translate-x-4 z-10"
+                type="button"
+                onClick={handleNavToggle}
+            >
+                <span
+                    className={`bg-black h-1 rounded-full transform-gpu transition transition-all duration-300 w-full origin-right ${firstSpanStyles}`}
                     isMobileNavOpen={isMobileNavOpen}
                     isScrolled={isScrolled}
                 />
-                <StyledSpan
+                <span
+                    className={`bg-black h-1 rounded-full transform-gpu transition transition-all duration-300 w-full origin-right ${secondSpanStyles}`}
                     isMobileNavOpen={isMobileNavOpen}
                     isScrolled={isScrolled}
                 />
-                <StyledSpan
+                <span
+                    className={`bg-black h-1 rounded-full transform-gpu transition transition-all duration-300 w-full origin-right ${thirdSpanStyles}`}
                     isMobileNavOpen={isMobileNavOpen}
                     isScrolled={isScrolled}
                 />
-            </NavButton>
-            <UL isMobileNavOpen={isMobileNavOpen}>
+            </button>
+            <ul
+                className={`absolute bg-white right-0 lg:hidden min-h-screen w-max flex flex-col items-end transform transform-gpu transition pr-4 pt-32 top-0 text-right w-1/2 ${navActiveStyles}`}
+            >
                 {navItems.map((item) => {
                     return (
-                        <LI key={item.name}>
+                        <li
+                            className="capitalize font-bold ml-8 mb-10 transition-colors duration-700 ease-in-out relative text-xl"
+                            key={item.name}
+                        >
                             <Link to={item.link}>{item.name}</Link>
-                        </LI>
+                        </li>
                     );
                 })}
-            </UL>
+            </ul>
         </>
     );
 }
