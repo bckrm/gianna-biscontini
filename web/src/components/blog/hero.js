1c270cd1349b8bf2ae0c4d4dfb8c2e4e158669d7
asasmith <smith.asa.la@gmail.com>
2021-03-27 23:06:05 -0400

    Update layout

--- web/src/components/blog/hero.js
+++ web/src/components/blog/hero.js
@@ -13,18 +13,18 @@ export default function Hero({ data }) {
         title,
     } = data;
     return (
-        <section className="container flex flex-col pt-32 relative">
+        <section className="container pt-32 mb-8">
             <Link
                 className="flex items-center text-brand-1 text-xl"
                 to="/writing"
             >
                 <MdKeyboardArrowLeft /> Back to blog index
             </Link>
-            <div className="relative py-32">
-                <h1 className="text-[89px] text-brand-1 leading-[125%] w-[60%]">
+            <div className="relative flex flex-col">
+                <h1 className="absolute left-0 transform -translate-y-1/2 top-1/2 w-[60%] text-[89px] text-brand-1 leading-[125%] w-[60%] z-10">
                     {title}
                 </h1>
-                <div className="absolute right-0 transform -translate-y-1/2 top-1/2 w-[60%] z-[-1]">
+                <div className="self-end transform translate-x-[2rem] w-[60%]">
                     <Img fluid={imageData} />
                 </div>
             </div>
