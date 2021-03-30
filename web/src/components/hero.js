0c513441f3f4a1a69de9effd484e44aa2d52efb9
asasmith <smith.asa.la@gmail.com>
2021-03-30 18:08:37 -0400

    Update layout for interior page hero's

--- web/src/components/hero.js
+++ web/src/components/hero.js
@@ -19,19 +19,28 @@ export default function Hero({ background, data, isContact }) {
         className={`translate-y-[${transform}]`}
     */
     // const transform = imageData ? 'translate-y-[50%]' : 'translate-y-[200%]';
-    const heroStyle = imageData ? 'mb-52 mt-80' : 'mb-28 mt-[25rem]';
+    const heroStyle = imageData ? 'mb-52 mt-8' : 'mb-28 mt-8';
 
     return (
-        <section className="relative">
-            <div className="absolute top-0 left-0 w-1/2 z-[-1]">
-                <Img fluid={bgImageData} />
+        <section>
+            <div className="relative overflow-hidden top-0 left-0 w-full md:w-3/4 lg:w-1/2 z-[-1]">
+                <div className="w-[125%] md:w-full">
+                    <Img
+                        critical
+                        durationFadeIn={0}
+                        fadeIn={false}
+                        fluid={bgImageData}
+                        loading="eager"
+                    />
+                    <h1 className="absolute w-full top-1/2 left-1/2 md:left-[5%] text-h1 text-center md:text-left text-white transform -translate-y-3/4 -translate-x-1/2 md:-translate-x-0 relative">
+                        {pageTitle}
+                    </h1>
+                </div>
             </div>
-            <h1 className="container text-h1 text-white transform translate-y-full">
-                {pageTitle}
-            </h1>
+
             {isContact ? (
                 <div
-                    className={`container ${heroStyle} transform md:translate-y-[-10rem]`}
+                    className={`container ${heroStyle} transform lg:mt-[-10rem]`}
                 >
                     <p className="block font-display text-h4 text-brand-2 ml-auto mb-10 md:w-1/2">
                         {heading}
@@ -41,12 +50,12 @@ export default function Hero({ background, data, isContact }) {
             ) : (
                 <div className={`container ${heroStyle}`}>
                     {imageData && (
-                        <div className="ml-auto mb-8 px-16 w-1/2">
+                        <div className="ml-auto mb-8 px-16 lg:w-1/2">
                             <Img fluid={imageData} />
                         </div>
                     )}
                     {heading && (
-                        <p className="block text-xl w-1/2 ml-auto px-16">
+                        <p className="block text-xl lg:w-1/2 ml-auto lg:px-16">
                             {heading}
                         </p>
                     )}
