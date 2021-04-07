a8e806507b3ef0b1034439b8fa6976bba012858d
asasmith <smith.asa.la@gmail.com>
2021-04-06 20:34:05 -0400

    Fix blog hero layout

--- web/src/components/blog/hero.js
+++ web/src/components/blog/hero.js
@@ -21,10 +21,10 @@ export default function Hero({ data }) {
                 <MdKeyboardArrowLeft /> Back to blog index
             </Link>
             <div className="relative flex flex-col">
-                <h1 className="absolute left-0 transform -translate-y-1/2 top-1/2 w-[60%] text-[89px] text-brand-1 leading-[125%] w-[60%] z-10">
+                <h1 className="lg:absolute lg:left-0 lg:transform lg:-translate-y-1/2 lg:top-1/2 lg:w-[60%] text-h1 lg:text-[89px] text-brand-1 text-center lg:text-leftleading-[125%] lg:w-[60%] z-10">
                     {title}
                 </h1>
-                <div className="self-end transform translate-x-[2rem] w-[60%]">
+                <div className="lg:self-end lg:transform lg:translate-x-[2rem] lg:w-[60%]">
                     <Img fluid={imageData} />
                 </div>
             </div>
