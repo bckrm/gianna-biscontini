cb9e40d96570d2821cdb0fc2219d13ef076756b7
asasmith <smith.asa.la@gmail.com>
2021-08-29 18:02:28 -0400

    Update spacing, add icon

--- web/src/components/intro.js
+++ web/src/components/intro.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import Img from 'gatsby-image';
+import SvgGroup6 from './svgs/group6';
 
 export default function Intro({ data }) {
     const {
@@ -15,6 +16,9 @@ export default function Intro({ data }) {
         <section className="container grid grid-cols-1 md:grid-cols-2 gap-10 mb-44">
             <Img fluid={imageData} />
             <div className="flex flex-col justify-end items-start md:-mb-16">
+                <div className="hidden lg:block transform -rotate-90">
+                    <SvgGroup6 />
+                </div>
                 <h2 className="mb-2 text-h3 text-brand-1">{introHeading}</h2>
                 <p className="text-xl">{introBody}</p>
             </div>
