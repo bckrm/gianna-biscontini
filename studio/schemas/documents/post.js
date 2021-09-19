0f9568713da1db6905d6c071eeab6b4b59865ab9
asasmith <smith.asa.la@gmail.com>
2021-09-19 17:42:22 -0400

    Hotfix: Make slug required field

--- studio/schemas/documents/post.js
+++ studio/schemas/documents/post.js
@@ -17,6 +17,7 @@ export default {
                 source: 'title',
                 maxLength: 96,
             },
+            validation: (Rule) => Rule.require(),
         },
         {
             name: 'publishDate',
